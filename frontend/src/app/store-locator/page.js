890bb0775eda152a6de19d9512a0c6b0b299ecf6
Nilakshhh <dnilaksh05@gmail.com>
2024-10-30 14:33:25 +0530

    map initialisation

---
+++ frontend/src/app/store-locator/page.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState, useMemo } from 'react';
+import dynamic from 'next/dynamic';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export default function About() {
+    return (
+        <>
+        <Navbar />
+        <div className='flex flex-row mt-[10vh]'>
+            <div className='w-[50%] h-[80vh]'>Accordion window</div>
+            <div className='w-[50%] h-[80vh] border-l-2 border-gray-500'>map window</div>
+
+        </div>
+        <Footer />
+        </>
+        
+    );
+}
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+
+  return (
+    <div className="absolute top-0 bg-[#30D5C8] w-[100%] h-[10vh]">
+
+    </div>
+  );
+}
