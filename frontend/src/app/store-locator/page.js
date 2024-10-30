25d50ddcac1dfc8d0a3e0f80ab90d7f99fbf1e97
Nilakshhh <dnilaksh05@gmail.com>
2024-10-30 18:31:32 +0530

    started map canvas

--- frontend/src/app/store-locator/page.js
+++ frontend/src/app/store-locator/page.js
@@ -4,6 +4,7 @@ import { useState, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Map from '@/components/Map';
 
 export default function About() {
     return (
@@ -11,7 +12,7 @@ export default function About() {
         <Navbar />
         <div className='flex flex-row mt-[10vh]'>
             <div className='w-[50%] h-[80vh]'>Accordion window</div>
-            <div className='w-[50%] h-[80vh] border-l-2 border-gray-500'>map window</div>
+            <div className='w-[50%] h-[80vh] border-l-2 border-gray-500'><Map /></div>
 
         </div>
         <Footer />
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
 
 const Map = () => {
   useEffect(() => {
-    return () => {
-      // Cleanup code if necessary
-    };
+    const mapContainer = document.getElementById('map');
+    if (mapContainer && mapContainer._leaflet_id) return;
+
+    // Initialize the map
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Custom icon setup
+    const customIcon = L.icon({
+      iconUrl: '/stock.svg', // Path to your custom icon image
+      iconSize: [80, 40], // Set the size of the icon (width, height)
+      iconAnchor: [15, 40], // Point of the icon which will correspond to marker's location
+      popupAnchor: [25, -40], // Position of the popup relative to the icon
+    });
+
+    L.marker([51.5, -0.09], { icon: customIcon }).addTo(map)
+      .bindPopup('A pretty CSS popup with a custom icon!')
+      .openPopup();
   }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+    <div id="map" style={{ height: "100%", width: "100%" }}></div>
   );
 };
 
