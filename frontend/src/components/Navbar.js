e7559d8d2091f7e61143141fc3fe0d0a335abfd3
Nilakshhh <dnilaksh05@gmail.com>
2024-11-03 12:07:37 +0530

    minor changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,6 +4,7 @@ export default function App() {
 
   return (
     <div className="absolute top-0 bg-[#30D5C8] w-[100%] h-[10vh]">
+      <img src="/logo.svg" className="w-[8vw] h-[10vh] pl-[12px]"></img>
 
     </div>
   );
