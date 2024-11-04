6366d3b903f61e8b1ebf9405711dc7463d1609f0
Nilakshhh <dnilaksh05@gmail.com>
2024-11-04 11:31:43 +0530

    deployment change

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Script from "next/script"; // Import Script from next/script
 import "./globals.css";
 
 const geistSans = localFont({
@@ -21,16 +22,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossorigin="" />
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-          crossorigin=""></script>
+          crossOrigin=""
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Add async Leaflet script */}
+        <Script
+          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossOrigin=""
+          strategy="beforeInteractive"
+        />
         {children}
       </body>
     </html>
